711fd4b08058a840eee4e701e925444a2cdcce18
heidegerblaga <skyrimpluta@gmail.com>
2023-02-17 01:02:43 +0100

    -a

--- exl.py
+++ exl.py
@@ -18,11 +18,12 @@ def Check(name,procedure):
 
 
 def count_commission():
+
     stawki = pd.read_excel("Draft.xlsx",sheet_name="Stawki prowizji",index_col = 1)
-    y = pd.read_excel("Draft.xlsx",sheet_name="Raport Spedytorzy - przyklad",skiprows=2, index_col = 1)
+    y = pd.read_excel("Draft.xlsx",sheet_name="Raport Spedytorzy - przyklad",skiprows=2, index_col = 1).drop(['Unnamed: 0'], axis=1)
     table = pd.merge(stawki, y, left_on="ID Spedytora", right_index=True, how="left", sort=False)
-    z = table[{"ID Teamu","ID kalkulacji","Prowizja","ID kalkulacji"}].drop_duplicates(keep="first").sort_values(by=['ID Spedytora'])
-    print(z)
+
+    z = table[{"ID Teamu","ID kalkulacji","Prowizja","ID kalkulacji"}].reset_index().drop_duplicates().reset_index().drop(['index'], axis=1)
 
 
     teams = z.drop_duplicates("ID Teamu")["ID Teamu"].size
@@ -32,14 +33,16 @@ def count_commission():
     T1 = lambda wynik, stawka, potracenia, dodatki: wynik * stawka - potracenia + dodatki
     T2 = lambda wynik, stawka, potracenia, dodatki: (wynik * stawka) * 0.82 - potracenia + dodatki
     T3 = lambda wynik, stawka, potracenia, dodatki, koszty: (wynik - koszty) * stawka - potracenia + dodatki
-    print(teams)
-    for i in range(1, teams):
+    p=1
+    for i in range(1, teams+1):
+
 
         for j in range(0, z[z["ID Teamu"] == i]["ID Teamu"].size):
-
-            prow = list(z[z["ID Teamu"] == i]["Prowizja"])[j]
+            prow = list(z[z["ID Teamu"] == i]["Prowizja"])[j-1]
             prog = list(stawki[(stawki.index == z[z["ID Teamu"] == i].index[j])]["Próg PLN"])
-            stopa = list(stawki[(stawki.index == z[z["ID Teamu"] == i].index[j])]["Stawka %"])[0]
+
+
+            stopa = list(stawki[(stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j])]["Stawka %"])[0]
 
             if ("S" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
 
@@ -47,7 +50,7 @@ def count_commission():
 
                 for k in range(1, len(prog)):
                     if (prog[k] - prow) < 0:
-                        stopa = list(stawki[(stawki.index == z[z["ID Teamu"] == i].index[j])]["Stawka %"])[k]
+                        stopa = list(stawki[(stawki.index == z[z["ID Teamu"] == i].index[j+1])]["Stawka %"])[k]
 
                 # print(list(z[z["ID Teamu"]==i]["Prowizja"])[j])
                 # print(list(stawki[(stawki.index==z[z["ID Teamu"]==i].index[j])]["Próg PLN"]))
@@ -72,12 +75,13 @@ def count_commission():
                # print(z[z["ID Teamu"] == i].index[j])
                # print(z[z["ID Teamu"] == i]["ID Teamu"])
 
-            d = {'ID teamu': [list(z[z["ID Teamu"] == i]["ID Teamu"])[j]],'ID': [z[z["ID Teamu"] == i].index[j]], 'wynik': [S(prow, stopa, 0, 0)] }
-            df = pd.DataFrame(data=d)
-            print(z[z["ID Teamu"] == i])
+            d = {'ID teamu': [list(z[z["ID Teamu"] == i]["ID Teamu"])[j]],'ID': [list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]],'id kalkulacji': [list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]],'procent': stopa ,'wynik': prow, 'wyplata': [S(prow, stopa, 0, 0)] }
+            df = pd.DataFrame(data=d).set_index('ID')
+           # print(list(z[z["ID Teamu"] == i]["ID Teamu"]))
 
             with pd.ExcelWriter("Draft.xlsx", mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer :
-                df.to_excel(writer,sheet_name="nowa",startrow=(j+1)*2)
+                df.to_excel(writer,sheet_name="nowa",startrow=(p))
+                p+=2
 
             #print(z[z["ID Teamu"] == i])
 
