44000a02149af8c237f4fcc4f923424f3f9f735e
heidegerblaga <skyrimpluta@gmail.com>
2023-02-20 16:05:47 +0100

    -a

--- exl.py
+++ exl.py
@@ -1,10 +1,41 @@
 import pandas as pd
-import openpyxl
+from openpyxl import load_workbook
+import os
 
 # pobieranie arkusza spedytora i poprawnie podstawy do obliczenia prowizji jeżeli coś się nie zgadza
 
-def Check(name,procedure):
-    x = pd.read_excel("Draft.xlsx", sheet_name="Raport Spedytor - przyklad", skiprows=2)
+
+def choice_path():
+
+
+    #os.chdir()
+
+    path = []
+
+    folder_path = 'arkusze'
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            print('Plik:', filename)
+            path.append('arkusze/'+filename)
+            wb = load_workbook(filename='arkusze/'+filename)
+            # UWAGA !!!!
+            # ARKUSZE MUSZĄ BYĆ USTAWIONĘ W NASTĘPUJĄCEJ KOLEJNOŚCI
+            # RAPORT SPEDYTORZY -> RAPORT SPEDYTOR -> STAWKI PROWIZJI
+            path.append(wb.sheetnames[1])
+            path.append(wb.sheetnames[2])
+            path.append(wb.sheetnames[3])
+
+        elif os.path.isdir(file_path):
+            print('Folder:', filename)
+
+
+    return path
+
+
+def check(x,y,name,procedure):
+
 
     podstawa = 0
 
@@ -16,26 +47,50 @@ def Check(name,procedure):
 
 
 
+    return podstawa
+
+
+
 
 def count_commission(z,stawki):
 
 
     teams = z.drop_duplicates("ID Teamu")["ID Teamu"].size
 
-    S = lambda wynik, stawka, potracenia, dodatki: wynik * stawka - potracenia + dodatki
-    T1 = lambda wynik, stawka, potracenia, dodatki: wynik * stawka - potracenia + dodatki
-    T2 = lambda wynik, stawka, potracenia, dodatki: (wynik * stawka) * 0.82 - potracenia + dodatki
-    T3 = lambda wynik, stawka, potracenia, dodatki, koszty: (wynik - koszty) * stawka - potracenia + dodatki
+    S = lambda wynik, stawka,sumapd: wynik * stawka + sumapd
+    T2 = lambda wynik, stawka,sumapd: (wynik * stawka) * 0.82 + sumapd
+    T3 = lambda wynik, stawka,sumapd, koszty: (wynik - koszty) * stawka + sumapd
     p=1
     for i in range(1, teams+1):
 
 
         for j in range(0, z[z["ID Teamu"] == i]["ID Teamu"].size):
+
             prow = list(z[z["ID Teamu"] == i]["Prowizja"])[j]
+
+            skladowa = 0.0
+            sumapd = 0.0
+            k = 1
+            while(k!=0):
+                print( list(z[z["ID Teamu"] == i]["Spedytor"])[j])
+                print("Dodaj potracenie/dodatek : ")
+                skladowa = float(input())
+                sumapd += skladowa
+                print("\n")
+                print('suma :')
+                print(sumapd)
+                print("\n")
+                print("Jezeli chcesz dodac kolejny koszt/potracenie nacisnij dowolna cyfre jezeli nie nacisnij 0")
+                k = int(input())
+                print("\n")
+
+
+
             prog = list(stawki[stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]]["Próg PLN"])
-            print(stawki[stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]]["Próg PLN"])
+            #print(stawki[stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]]["Próg PLN"])
             wyplata = 0
             stopa = 0
+            koszty = 0
 
             if ("S" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
                 for k in range(0, len(prog)):
@@ -45,37 +100,43 @@ def count_commission(z,stawki):
                         stopa = list(stawki[stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]]["Stawka %"])[k]
 
 
-                wyplata = S(prow, stopa, 0, 0)
+
+
+                wyplata = S(prow, stopa, sumapd)
 
 
             if ("T" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
 
                 prow = z[z["ID Teamu"] == i]["Prowizja"].sum()
 
-                print(list(z[z["ID Teamu"] == i]["ID Spedytora"])[j])
+                #print(list(z[z["ID Teamu"] == i]["ID Spedytora"])[j])
                 for k in range(0, len(prog)):
 
                     if (prog[k] - prow) < 0:
 
                         stopa = list(stawki[stawki.index == list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]]["Stawka %"])[k]
-                        print(stopa)
-                        print("*****")
+                        #print(stopa)
+                        #print("*****")
 
                 if ("T1" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
-                    wyplata = T1(prow, stopa, 0, 0)
-                if ("T2" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
-                    wyplata = T2(prow, stopa, 0, 0)
-                if ("T3" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
-                    wyplata = T3(prow, stopa, 0, 0,0)
 
-            d = {'ID teamu': [list(z[z["ID Teamu"] == i]["ID Teamu"])[j]],'ID': [list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]],'id kalkulacji': [list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]],'procent': stopa ,'wynik': prow, 'wyplata': [wyplata]}
+                    wyplata = S(prow, stopa, sumapd)
+                if ("T2" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
+                    wyplata = T2(prow, stopa, sumapd)
+                if ("T3" in list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]):
+                    print("Dodaj koszt teamu : ")
+                    input(koszty)
+                    wyplata = T3(prow, stopa,sumapd,koszty)
+
+            d = {'Spedytor': [list(z[z["ID Teamu"] == i]["Spedytor"])[j]] ,'ID teamu': [list(z[z["ID Teamu"] == i]["ID Teamu"])[j]],'ID': [list(z[z["ID Teamu"] == i]["ID Spedytora"])[j]],'id kalkulacji': [list(z[z["ID Teamu"] == i]["ID kalkulacji"])[j]],'procent': stopa ,'wynik': prow, 'wyplata': [wyplata]}
             df = pd.DataFrame(data=d).set_index('ID')
 
 
 
-            with pd.ExcelWriter("Draft.xlsx", mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer :
+            with pd.ExcelWriter("arkusze/Draft.xlsx", mode="a", engine="openpyxl", if_sheet_exists="overlay") as writer :
                 df.to_excel(writer,sheet_name="wyplaty",startrow=(p))
                 p+=2
+            print("Dodano !")
 
             #print(z[z["ID Teamu"] == i])
 
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from exl import count_commission
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from exl import count_commission ,check,choice_path
+import pandas as pd
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    count_commission()
+
+    name = choice_path()
+    stawki = pd.read_excel(name[0], sheet_name=name[3], index_col=1)
+    x = pd.read_excel(name[0], sheet_name=name[2], skiprows=2)
+    y = pd.read_excel(name[0], sheet_name=name[1], skiprows=2, index_col=0)
+    table = pd.merge(stawki, y, left_on="ID Spedytora", right_index=True, how="left", sort=False)
+    z = table[{"ID Teamu", "ID kalkulacji", "Prowizja", "ID kalkulacji",
+               "Spedytor"}].reset_index().drop_duplicates().reset_index().drop(['index'], axis=1)
+
+    z.at[ 6, "Prowizja"] = check(x,z,"plastik",lambda a: a*(0.5))
+    count_commission(z,stawki)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
